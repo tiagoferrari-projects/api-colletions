60484305f85a04a78c34d71d4adc99016a90dc4b
Tiago Ferrari <tiagoferrari77@gmail.com>
2021-02-26 23:55:42 -0300

    created separator text variable

--- src/main/kotlin/one/digitalinnovation/collection/TesteIntArray.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteIntArray.kt
@@ -2,6 +2,7 @@ package one.digitalinnovation.collection
 
 fun main() {
     val values = IntArray(6)
+    val separador = "------------------"
 
     values[0] = 7
     values[1] = 8
@@ -9,25 +10,25 @@ fun main() {
     values[3] = 2
     values[4] = 8
     values[5] = 25
-    //
+
     for(valor in values){
         println(valor)
     }
-    //
-    println("-----------------------------")
-    //
+
+    println(separador)
+
     values.forEach {valor ->
         println(valor)
     }
-    //
-    println("-----------------------------")
-    //
+
+    println(separador)
+
     for(index in values.indices){
         println(values[index])
     }
-    //
-    println("-----------------------------")
-    //
+
+    println(separador)
+
     values.sort()
     for(valor in values){
         println(valor)
