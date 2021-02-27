cc8aacb5f45973c4af1d4c8c9fa77b8331614b39
Tiago Ferrari <tiagoferrari77@gmail.com>
2021-02-26 23:19:26 -0300

    Initial commit

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteIntArray.kt
@@ -0,0 +1,35 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val values = IntArray(6)
+
+    values[0] = 7
+    values[1] = 8
+    values[2] = 27
+    values[3] = 2
+    values[4] = 8
+    values[5] = 25
+
+    for(valor in values){
+        println(valor)
+    }
+
+    println("-----------------------------")
+
+    values.forEach {valor ->
+        println(valor)
+    }
+
+    println("-----------------------------")
+
+    for(index in values.indices){
+        println(values[index])
+    }
+
+    println("-----------------------------")
+
+    values.sort()
+    for(valor in values){
+        println(valor)
+    }
+}
